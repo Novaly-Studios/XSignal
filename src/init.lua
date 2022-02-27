794b47fa0131ecb5b4873597c546aab2ea3afea2
adammabin <adam.mabin@gmail.com>
2022-02-27 04:27:46 +0000

    Fix timeout

--- src/init.lua
+++ src/init.lua
@@ -1,11 +1,50 @@
 --!nonstrict
-local Connection = require(script.Connection)
+--local Connection = require(script.Connection)
+
+export type Connection = {
+    Connected: boolean,
+
+    Disconnect: ((Connection) -> ()),
+    Reconnect: ((Connection) -> ()),
+}
 
 local function BLANK_FUNCTION() end
 
-local DEFAULT_WAIT_TIMEOUT_SECONDS = 30
-local DEFAULT_AWAIT_ALL_TIMEOUT_SECONDS = 30
+local Connection = {}
+Connection.__index = Connection
+
+function Connection.new()
+    return setmetatable({
+        Connected = false;
+
+        _DisconnectCallback = BLANK_FUNCTION;
+        _ReconnectCallback = BLANK_FUNCTION;
+    }, Connection)
+end
+
+function Connection:Disconnect()
+    if (not self.Connected) then
+        return
+    end
+
+    self.Connected = false
+    self._DisconnectCallback()
+end
+
+function Connection:Reconnect()
+    if (self.Connected) then
+        return
+    end
+
+    self.Connected = true
+    self._ReconnectCallback()
+end
+
+local function BLANK_FUNCTION() end
+
 local DEFAULT_AWAIT_FIRST_TIMEOUT_SECONDS = 30
+local DEFAULT_AWAIT_ALL_TIMEOUT_SECONDS = 30
+local DEFAULT_WAIT_TIMEOUT_SECONDS = 30
 
 local ERR_CONNECTION_ALREADY_CREATED = "Connection already created in slot %d"
 local INVALID_ARGUMENT = "Invalid argument #%d (%s expected, got %s)"
@@ -13,6 +52,11 @@ local ERR_WAIT_TIMEOUT = "Wait call timed out (time elapsed: %d)"
 local ERR_NO_SIGNALS = "No Signals passed"
 
 local COROUTINE_SUSPENDED = "suspended"
+
+local TYPE_FUNCTION = "function"
+local TYPE_BOOLEAN = "boolean"
+local TYPE_NUMBER = "number"
+local TYPE_TABLE = "table"
 
 type Signal<T...> = {
     Connect: ((Signal<T...>, ((T...) -> ())) -> (Connection.Connection)),
@@ -30,7 +74,7 @@ Signal.__index = Signal
 
 function Signal.new(ImmediateFire)
     if (ImmediateFire) then
-        CheckType(ImmediateFire, 1, "function")
+        CheckType(ImmediateFire, 1, TYPE_FUNCTION)
     end
 
     local self = setmetatable({
@@ -40,7 +84,8 @@ function Signal.new(ImmediateFire)
         _AwaitingCoroutines = {};
 
         _ImmediateFire = ImmediateFire;
-        _OnDestroy = BLANK_FUNCTION;
+        _OnConnectionsEmpty = BLANK_FUNCTION;
+        _OnConnectionsPresent = BLANK_FUNCTION;
     }, Signal)
 
     self.Event = self -- Easy to port BindableEvents over in existing codebases
@@ -50,22 +95,28 @@ end
 
 --- Creates a new connection object given a callback function, which is called when the Signal is fired.
 function Signal:Connect(Callback)
-    CheckType(Callback, 1, "function")
+    CheckType(Callback, 1, TYPE_FUNCTION)
 
     local NewConnection = Connection.new()
 
     NewConnection._DisconnectCallback = function()
-        --self._ConnectionCallbacks[Callback] = nil
         local ConnectionCallbacks = self._ConnectionCallbacks
         table.remove(ConnectionCallbacks, table.find(ConnectionCallbacks, Callback))
         self.ConnectionCount -= 1
+
+        if (self.ConnectionCount == 0) then
+            self._OnConnectionsEmpty()
+        end
     end
 
     NewConnection._ReconnectCallback = function()
-        --self._ConnectionCallbacks[Callback] = true
         local ConnectionCallbacks = self._ConnectionCallbacks
         table.insert(ConnectionCallbacks, Callback)
         self.ConnectionCount += 1
+
+        if (self.ConnectionCount == 1) then
+            self._OnConnectionsPresent()
+        end
     end
 
     NewConnection:Reconnect()
@@ -82,7 +133,7 @@ function Signal:Connect(Callback)
 end
 Signal.connect = Signal.Connect
 
---- Fires the Signal, calling all connected callbacks in their own coroutine. This is not ordered.
+--- Fires the Signal, calling all connected callbacks in their own coroutine.
 function Signal:Fire(...)
     -- Resume yielded coroutines
     for _, Awaiting in ipairs(self._AwaitingCoroutines) do
@@ -101,27 +152,34 @@ Signal.fire = Signal.Fire
 --- Yields the current coroutine until the Signal is fired, returning all data passed when the Signal was fired.
 function Signal:Wait(Timeout, ThrowErrorOnTimeout)
     Timeout = Timeout or DEFAULT_WAIT_TIMEOUT_SECONDS
-    CheckType(Timeout, 1, "number")
+    CheckType(Timeout, 1, TYPE_NUMBER)
 
     if (ThrowErrorOnTimeout) then
-        CheckType(ThrowErrorOnTimeout, 2, "boolean")
+        CheckType(ThrowErrorOnTimeout, 2, TYPE_BOOLEAN)
     end
 
-    local Temp = self:Connect(BLANK_FUNCTION) -- Need to do this for extended signals otherwise they are immediate disconnects
+    local AwaitingCoroutines = self._AwaitingCoroutines
+    local Temp = self:Connect(BLANK_FUNCTION) -- Need to do this for extended Signals otherwise they are immediate disconnects
     local ActiveCoroutine = coroutine.running()
-    table.insert(self._AwaitingCoroutines, ActiveCoroutine)
+    table.insert(AwaitingCoroutines, ActiveCoroutine)
 
     local DidTimeout = false
+    local DidResume = false
 
     task.delay(Timeout, function()
-        if (coroutine.status(ActiveCoroutine) == COROUTINE_SUSPENDED) then
-            DidTimeout = true
-            Temp:Disconnect() -- Safe for coroutine.close to be called
-            task.spawn(ActiveCoroutine)
+        -- Could time out at a later point, so once we resume we know it is only yielding for this & can reject in future
+        if (DidResume) then
+            return
         end
+
+        DidTimeout = true
+        Temp:Disconnect() -- Safe for coroutine.close to be called
+        table.remove(AwaitingCoroutines, table.find(AwaitingCoroutines, ActiveCoroutine))
+        task.spawn(ActiveCoroutine)
     end)
 
     local Result = {coroutine.yield()}
+    DidResume = true
     Temp:Disconnect()
 
     if (DidTimeout and ThrowErrorOnTimeout) then
@@ -136,7 +194,7 @@ Signal.wait = Signal.Wait
 function Signal:Destroy()
     self._ConnectionCallbacks = {}
     self.ConnectionCount = 0
-    self._OnDestroy()
+    self._OnConnectionsEmpty()
 end
 Signal.destroy = Signal.Destroy
 Signal.DisconnectAll = Signal.Destroy
@@ -145,12 +203,12 @@ Signal.disconnectAll = Signal.Destroy
 --- Awaits the completion of the first Signal object and returns its fired data.
 function Signal.AwaitFirst(Signals: {Signal<any>}, Timeout: number?, ThrowErrorOnTimeout: boolean?): ...any
     Timeout = Timeout or DEFAULT_AWAIT_FIRST_TIMEOUT_SECONDS
-    CheckType(Timeout, 2, "number")
-    CheckType(Signals, 1, "table")
+    CheckType(Timeout, 2, TYPE_NUMBER)
+    CheckType(Signals, 1, TYPE_TABLE)
     assert(#Signals > 0, ERR_NO_SIGNALS)
 
     if (ThrowErrorOnTimeout) then
-        CheckType(ThrowErrorOnTimeout, 3, "boolean")
+        CheckType(ThrowErrorOnTimeout, 3, TYPE_BOOLEAN)
     end
 
     local ActiveCoroutine = coroutine.running()
@@ -163,20 +221,24 @@ function Signal.AwaitFirst(Signals: {Signal<any>}, Timeout: number?, ThrowErrorO
     end
 
     local DidTimeout = false
+    local DidResume = false
 
     task.delay(Timeout, function()
-        if (coroutine.status(ActiveCoroutine) == COROUTINE_SUSPENDED) then
-            DidTimeout = true
-
-            for _, SubConnection in ipairs(Connections) do
-                SubConnection:Disconnect()
-            end
-
-            task.spawn(ActiveCoroutine)
+        if (DidResume) then
+            return
         end
+
+        DidTimeout = true
+
+        for _, SubConnection in ipairs(Connections) do
+            SubConnection:Disconnect()
+        end
+
+        task.spawn(ActiveCoroutine)
     end)
 
     local Result = {coroutine.yield()}
+    DidResume = true
 
     for _, SubConnection in ipairs(Connections) do
         SubConnection:Disconnect()
@@ -194,12 +256,12 @@ Signal.awaitFirst = Signal.AwaitFirst
 --- Return order is maintained for the Signals passed in.
 function Signal.AwaitAll(Signals: {Signal<any>}, Timeout: number?, ThrowErrorOnTimeout: boolean?): {{any}}
     Timeout = Timeout or DEFAULT_AWAIT_ALL_TIMEOUT_SECONDS
-    CheckType(Timeout, 2, "number")
-    CheckType(Signals, 1, "table")
+    CheckType(Timeout, 2, TYPE_NUMBER)
+    CheckType(Signals, 1, TYPE_TABLE)
     assert(#Signals > 0, ERR_NO_SIGNALS)
 
     if (ThrowErrorOnTimeout) then
-        CheckType(ThrowErrorOnTimeout, 3, "boolean")
+        CheckType(ThrowErrorOnTimeout, 3, TYPE_BOOLEAN)
     end
 
     local TargetCount = #Signals
@@ -209,17 +271,20 @@ function Signal.AwaitAll(Signals: {Signal<any>}, Timeout: number?, ThrowErrorOnT
     local Count = 0
 
     local DidTimeout = false
+    local DidResume = false
 
     task.delay(Timeout, function()
-        if (coroutine.status(ActiveCoroutine) == COROUTINE_SUSPENDED) then
-            DidTimeout = true
-
-            for _, SubConnection in ipairs(Connections) do
-                SubConnection:Disconnect()
-            end
-
-            task.spawn(ActiveCoroutine)
+        if (DidResume) then
+            return
         end
+
+        DidTimeout = true
+
+        for _, SubConnection in ipairs(Connections) do
+            SubConnection:Disconnect()
+        end
+
+        task.spawn(ActiveCoroutine)
     end)
 
     for Index, Value in ipairs(Signals) do
@@ -237,6 +302,7 @@ function Signal.AwaitAll(Signals: {Signal<any>}, Timeout: number?, ThrowErrorOnT
     end
 
     coroutine.yield()
+    DidResume = true
 
     if (DidTimeout and ThrowErrorOnTimeout) then
         error(ERR_WAIT_TIMEOUT:format(Timeout))
@@ -262,31 +328,38 @@ Signal.awaitAllFirstArg = Signal.AwaitAllFirstArg
 
 --- Watches multiple other Signal objects and replicates firing through any of them.
 function Signal.Extend(Signals: {Signal<any>}, ...)
-    CheckType(Signals, 1, "table")
+    CheckType(Signals, 1, TYPE_TABLE)
     assert(#Signals > 0, ERR_NO_SIGNALS)
 
     local NewSignal = Signal.new(...)
     local ConnectionsList = table.create(#Signals)
 
-    -- Hook into all provided signals
-    for Index, SubSignal in ipairs(Signals) do
-        assert(ConnectionsList[Index] == nil, ERR_CONNECTION_ALREADY_CREATED:format(Index))
-
-        ConnectionsList[Index] = SubSignal:Connect(function(...)
-            NewSignal:Fire(...)
-        end)
-    end
-
     -- Unhook provided signals on object destruction
-    NewSignal._OnDestroy = function()
+    NewSignal._OnConnectionsEmpty = function()
         for Index, SubConnection in ipairs(ConnectionsList) do
             SubConnection:Disconnect()
             ConnectionsList[Index] = nil
         end
     end
 
+    -- Hook into all provided signals
+    NewSignal._OnConnectionsPresent = function()
+        for Index, SubSignal in ipairs(Signals) do
+            assert(ConnectionsList[Index] == nil, ERR_CONNECTION_ALREADY_CREATED:format(Index))
+
+            ConnectionsList[Index] = SubSignal:Connect(function(...)
+                NewSignal:Fire(...)
+            end)
+        end
+    end
+
     return NewSignal
 end
 Signal.extend = Signal.Extend
+
+--- Same as Signal.Extend, but with a single Signal object.
+function Signal.Wrap(SignalObject: Signal<any>, ...)
+    return Signal.Extend({SignalObject}, ...)
+end
 
 return Signal
